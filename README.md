98d8e704d78e7177f05c7b172a22e12af994a08d
Denis Mukonin <dezmukonin16@gmail.com>
2026-01-26 23:48:04 +0300

    feat: Initializing the Project

--- README.md
+++ README.md
@@ -2,6 +2,3 @@
 
 
 ##  Analytics & Data Visualization Dashboard — аналитический дашборд для отслеживания KPI и метрик приложения с интерактивными графиками, фильтрацией по датам и real-time обновлениями.
-
-
-# Analytics-Data-Visualization-Dashboard
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Analytics & Data Visualization Dashboard</h1>
+      <p className="read-the-docs">
+      Analytics & Data Visualization Dashboard — аналитический дашборд для отслеживания KPI и метрик приложения с интерактивными графиками, фильтрацией по датам и real-time обновлениями.
+      </p>
+    </>
+  )
+}
+
+export default App
