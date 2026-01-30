71890ff59b6dbf2a71cff92ba61af277a8c6b474
Denis Mukonin <dezmukonin16@gmail.com>
2026-01-30 06:45:48 +0300

    feat: settings taiwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Analytics & Data Visualization Dashboard</h1>
-      <p className="read-the-docs">
-      Analytics & Data Visualization Dashboard — аналитический дашборд для отслеживания KPI и метрик приложения с интерактивными графиками, фильтрацией по датам и real-time обновлениями.
-      </p>
-    </>
+    <div className=''>
+      <header className='header'>
+        <h1>Analytics & Data Visualization Dashboard</h1>
+      </header>
+      <main className='main'>
+        <p className="text-3xl font-bold text-blue-800">
+          Analytics & Data Visualization Dashboard — аналитический дашборд для отслеживания KPI и метрик приложения с интерактивными графиками, фильтрацией по датам и real-time обновлениями.
+        </p>
+      </main>
+      <footer>
+        <p> Тут будет footer</p>
+      </footer>
+    </div>
   )
 }
 
